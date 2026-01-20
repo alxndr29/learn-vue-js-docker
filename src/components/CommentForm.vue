e5701964a5ef07c1b52ca1cfec20f508366edfb7
Alexander Evan <61173884+alxndr29@users.noreply.github.com>
2026-01-20 23:19:02 +0700

    push

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -2,7 +2,7 @@
 import customApi from '@/utils/api'
 import { useMutation, useQueryClient } from '@tanstack/vue-query'
 import { useAuthStore } from '@/store/auth'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const queryClient = useQueryClient()
 const authStore = useAuthStore()
@@ -32,7 +32,7 @@ const createOrUpdateCommentMutation = useMutation({
         )
     },
     onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["ArticleDataDetail"] })
+        queryClient.invalidateQueries({ queryKey: ["ArticleDataDetail","ArticleCOmmentStatus"] })
         content.value = ""
     },
     onError: (error) => {
@@ -43,13 +43,21 @@ const handleSubmit = () => {
     createOrUpdateCommentMutation.mutate()
     alert('submit komen berhasil')
 }
+
+onMounted(() => {
+    if(props.isUpdate && props.content){
+        content.value = props.content
+    }
+})
 </script>
 <template>
-    <fieldset class="fieldset bg-base-300 my-3 border-info roundex-box border p-4">
+    <fieldset class="fieldset my-3 p-4 bg-base-300">
         <form @submit.prevent="handleSubmit">
             <textarea class="textarea w-full h-32" placeholder="Isi Komentar Anda" v-model="content"></textarea>
             <div class="flex justify-end">
-                <button class="btn btn-primary my-3" type="submit">Submit</button>
+                <button class="btn btn-primary my-3" :class="props.isUpdate ? 'btn-info' : 'btn-primary'" type="submit">
+                    {{ props.isUpdate ? "Update" : "Create" }}
+                </button>
             </div>
         </form>
 
--- src/views/DetailArticle.vue
+++ src/views/DetailArticle.vue
@@ -5,16 +5,45 @@ import customApi from "@/utils/api.ts";
 import Error from "@/components/Error.vue";
 import Loading from "@/components/Loading.vue";
 import CommentForm from "@/components/CommentForm.vue";
+import { useAuthStore } from "@/store/auth";
+import { ref } from "vue";
+
+const isUpdate = ref<boolean>(false)
+const handleUpdate = () => {
+  isUpdate.value = true
+}
+const handleCloseUpdate = () => {
+  isUpdate.value = false
+}
+
 let route = useRoute();
+const authStore = useAuthStore();
 
 const fetchData = async () => {
   const { data } = await customApi.get(`/article/${route.params.id}`)
   return data;
 };
 
+
+
 const { data, isLoading, error } = useQuery({
   queryKey: ["ArticleDataDetail"],
   queryFn: fetchData,
+});
+
+const fetchDataArticleComment = async () => {
+  const { data } = await customApi.get(`/comment/${route.params.id}`, {
+    headers: {
+      Authorization: `Bearer ${authStore.token}`
+    }
+  })
+  console.log(data)
+  return data;
+};
+const { data: dataComment, isLoading: isLoadingComment, error: errorComment } = useQuery({
+  queryKey: ["ArticleCOmmentStatus"],
+  queryFn: fetchDataArticleComment,
+  enabled: !!authStore.token
 });
 
 </script>
@@ -45,7 +74,13 @@ const { data, isLoading, error } = useQuery({
         {{ articleTag.tag.name }}
       </div>
     </section>
-    <CommentForm :ArticleId="route.params.id" :isUpdate="false" />
+    <div v-if="isLoadingComment">
+      <Loading /> Loading ...
+    </div>
+    <div v-else-if="dataComment && !dataComment.status">
+      <CommentForm :ArticleId="route.params.id" :isUpdate="false" />
+    </div>
+
     <section class="my-5">
       <h1 class="text-2xl font-bold text-info my-2">List Comment:</h1>
       <div class="card card-border border-info bg-base-300" v-for="comment in data.comments"
@@ -56,7 +91,16 @@ const { data, isLoading, error } = useQuery({
           <small class="text-xs">
             {{ new Date(comment.createdAt).toDateString() }}
           </small>
+          <div class="flex justify-end" v-if="dataComment && dataComment.status && dataComment.id === comment.id">
+            <button class="btn btn-accent btn-md" v-if="!isUpdate" @click="handleUpdate">Update</button>
+            <button class="btn btn-accent btn-md" v-else @click="handleCloseUpdate">Cancel</button>
+          </div>
+          <div v-if="isUpdate && comment.id === dataComment.id">
+            <CommentForm :ArticleId="route.params.id" :isUpdate="true" :content="comment.content" />
+          </div>
+
         </div>
+
       </div>
       <div v-else>
         <h1 class="text-xl font-bold">Belum ada komentar</h1>
